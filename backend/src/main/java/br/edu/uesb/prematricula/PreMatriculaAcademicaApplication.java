083b3fd9b47a1cc19ee56cb5aa2ea8df61227886
Vinicius Souza <viniciusouza7@gmail.com>
2026-07-04 04:20:08 -0300

    feat: setup initial backend infrastructure with docker, postgres and swagger

---
+++ backend/src/main/java/br/edu/uesb/prematricula/PreMatriculaAcademicaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.uesb.prematricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreMatriculaAcademicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreMatriculaAcademicaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/edu/uesb/prematricula/PreMatriculaAcademicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.uesb.prematricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreMatriculaAcademicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
